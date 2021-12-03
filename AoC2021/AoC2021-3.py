814b792322ba9403ea09934e89432f6f4ba1fa54
Martin Døhl-Aagnes <martindaa@hotmail.no>
2021-12-03 15:32:46 +0100

    Day3

---
+++ AoC2021/AoC2021-3.py
@@ -0,0 +1,17 @@
+from os import close
+
+
+#def day3_1(inn):
+    
+
+            
+if __name__ == '__main__':
+    input =[]
+    i=0
+    f = open('AoC2021/Input/day3.txt', 'r')
+    fil = f.read()
+    f.close()
+
+    input = fil.split('\n')
+
+    #day3_1(input) 
